7a66d2d6b5a68175fa38845baed2680153024612
WhispersofHeaven <dimareshka1711@gmail.com>
2026-09-20 14:42:44 +0700

    Add files via upload

--- lesson1.cpp
+++ lesson1.cpp
@@ -1,8 +1,8 @@
-﻿/*************************
+﻿/****************************
 * Автор: Решетников Дмитрий *
-* Вариант: 6 *
-* Название: Линейные *
-*************************/
+* Вариант: 6                *
+* Название: Линейные        *
+****************************/
 #include <iostream>
 using namespace std;
 
@@ -12,19 +12,21 @@ int main() {
 
   cout << "Side a: ";
   cin >> a;
+
   cout << "Side b: ";
   cin >> b;
+  
   cout << "Side c: ";
   cin >> c;
 
-  p = (a + b + c) / 2;
+  p = (a + b + c) / 2.0;
   A = acos((b * b + c * c - a * a) / (2 * b * c));
   B = asin((b * sin(A)) / a);
-  C = 2*atan(sqrt(((p-a)*(p-b))/(p*(p-c))));
+  C = 2 * atan(sqrt(((p - a) * (p - b)) / (p * (p - c))));
 
   cout << "Angle A: " << A << endl
-	     << "Angle B: " << B << endl 
-	     << "Angle C: " << C << endl;
+    << "Angle B: " << B << endl
+    << "Angle C: " << C << endl;
 
   return 0;
 }
